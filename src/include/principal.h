03e93c7ec65640ea528a956b0070a12d9e95d26f
VSCODE <ejcl30@hotmail.com>
2022-02-26 16:51:22 +0100

    c3

--- src/include/principal.h
+++ src/include/principal.h
@@ -1,4 +1,5 @@
 #ifndef PRINCIPAL_H
 #define PRINCIPAL_H
     #define NUM 6
+    #define NUM2 20
 #endif /* PRINCIPAL_H */
--- src/principal.c
+++ src/principal.c
@@ -6,5 +6,13 @@ int main(int argc, char const *argv[])
     int i = NUM;
     printf("Número :");
     scanf("%d", &i);
+    printf("i=%d\n",i);
+    if (i>NUM2)
+    {
+        printf("Mayor de %d\n", NUM2);
+    } else {
+        printf("Menor de %d\n", NUM2);
+    }
+    
     return 0;
 }
